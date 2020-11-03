2b8ba85bef812e8754da8716e808314c30098a9e
roeylubin <73183791+roeylubin@users.noreply.github.com>
2020-11-03 20:08:42 +0200

    added js

---
+++ index.js
@@ -0,0 +1,9 @@
+function hoverTrue() {
+  document.querySelector('.dropbtn').style.backgroundColor = "#A7A774";
+  document.querySelector('.dropbtn').style.color = "white";
+}
+
+function hoverFalse() {
+  document.querySelector('.dropbtn').style.backgroundColor = "#CECE99";
+  document.querySelector('.dropbtn').style.color = "black";
+}
